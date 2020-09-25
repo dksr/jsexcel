e6d9a17c4b1c90ad13163ef0000011634bea64ca
Dmitry <dmitrij.krasnov@gmail.com>
2020-09-25 18:18:22 +0300

    Row and columns resizer

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,6 +2,11 @@ import {ExcelComponent} from '../../core/ExcelComponent'
 import {createTable} from './table.template'
 
 export class Table extends ExcelComponent {
+  constructor($root) {
+    super($root, {
+      listeners: [],
+    })
+  }
   static className = 'excel__table'
   toHTML() {
     return createTable()
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -11,14 +11,22 @@ function toCell(content = '') {
 
 function toColumn(col) {
   return `
-    <div class="column">${col}</div>
+    <div class="column">
+      ${col}
+      <div class="col-resize"></div>
+    </div>
   `
 }
 
 function createRow(index = '', content) {
+  const resize = index ? '<div class="row-resize"></div>' : ''
+
   return `
     <div class="row">
-      <div class="row-info">${index ? index : ''}</div>
+      <div class="row-info">
+        ${index ? index : ''}
+        ${resize}        
+      </div>
       <div class="row-data">${content}</div>
     </div>
   `
