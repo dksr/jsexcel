ff3ce7346281143efaa91923e1eefd61ce5623a7
dksr <61501278+dksr@users.noreply.github.com>
2020-09-30 17:07:37 +0300

    Table resize (#4)
    * Row and columns resizer
    
    * Finish resize

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,22 +3,30 @@ const CODES = {
   Z: 90,
 }
 
-function toCell(content = '') {
+function toCell(_, index) {
   return `
-  <div class="cell" contenteditable>${content}</div>
+  <div class="cell" contenteditable data-col="${index}"></div>
   `
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-    <div class="column">${col}</div>
+    <div class="column" data-type="resizable" data-col="${index}">
+      ${col}
+      <div class="col-resize" data-resize="col"></div>
+    </div>
   `
 }
 
 function createRow(index = '', content) {
+  const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
+
   return `
-    <div class="row">
-      <div class="row-info">${index ? index : ''}</div>
+    <div class="row" data-type="resizable">
+      <div class="row-info">
+        ${index ? index : ''}
+        ${resize}        
+      </div>
       <div class="row-data">${content}</div>
     </div>
   `
@@ -28,7 +36,7 @@ function toChar(_, index) {
   return String.fromCharCode(CODES.A + index)
 }
 
-export function createTable(rowsCount = 15) {
+export function createTable(rowsCount = 150) {
   const colsCount = CODES.Z - CODES.A + 1
   const rows = []
 
