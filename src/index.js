d1814968f90f9433b54845a4aa3aaba046b4a13b
Dmitry <dmitrij.krasnov@gmail.com>
2020-10-15 18:30:51 +0300

    Finish routing

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
-import {Excel} from './components/excel/Excel'
-import {Header} from '@/components/header/Header'
-import {Toolbar} from '@/components/Toolbar/toolbar'
-import {Formula} from '@/components/Formula/formula'
-import {Table} from '@/components/Table/table'
+import {Router} from './core/routes/Router'
+import {DashboardPage} from './pages/DashboardPage'
+import {ExcelPage} from './pages/ExcelPage'
+
 import './scss/index.scss'
-import {createStore} from './core/createStore'
-import {rootReducer} from './redux/rootReducer'
-import {storage, debounce} from './core/utils'
-import {initialState} from './redux/initialState'
 
-const store = createStore(rootReducer, initialState)
-
-const stateListener = debounce((state) => {
-  storage('excel-state', state)
-}, 300)
-
-store.subscribe(stateListener)
-
-const excel = new Excel('#app', {
-  components: [Header, Toolbar, Formula, Table],
-  store,
+new Router('#app', {
+  dashboard: DashboardPage,
+  excel: ExcelPage,
 })
-
-excel.render()
--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,6 +1,4 @@
-import {storage} from '../core/utils'
-import {defaultStyles} from '@/constants'
-import {defaultTitle} from '../constants'
+import {defaultStyles, defaultTitle} from '@/constants'
 
 const defaultState = {
   rowState: {},
@@ -10,6 +8,7 @@ const defaultState = {
   currentText: '',
   currentStyles: defaultStyles,
   title: defaultTitle,
+  openedDate: new Date().toJSON(),
 }
 
 const normalize = (state) => ({
@@ -18,6 +17,6 @@ const normalize = (state) => ({
   currentText: '',
 })
 
-export const initialState = storage('excel-state') ?
-  normalize(storage('excel-state')) :
-  defaultState
+export function normalizeInitialState(state) {
+  return state ? normalize(state) : defaultState
+}
